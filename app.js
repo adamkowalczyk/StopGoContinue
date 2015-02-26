8b59148c81ac46404fa6b6d3dec4375341b98f75
Adam Kowalczyk <adamkowalczyk@gmail.com>
2015-02-26 10:40:01 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,114 @@
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var url = require('url');
+var Twitter = require('twitter');
+var ecstatic = require('ecstatic')({root: __dirname + '/public'});
+
+// Check for config.json in root
+var configCheck = true;
+var config;
+try {
+	fs.readFileSync(path.join(__dirname,'config.json'));
+}
+catch (e) {
+	configCheck = false;
+}
+if (configCheck) {
+	config = require('./config.json');
+}
+
+// Instantiate client 
+var client;
+if (configCheck) {
+	client = new Twitter({
+		consumer_key: config.cKey,
+		consumer_secret: config.cSecret,
+		access_token_key: config.atKey,
+		access_token_secret: config.atSecret
+	});
+}
+else {
+	client = new Twitter({
+		consumer_key: process.env.C_KEY,
+		consumer_secret: process.env.C_SECRET,
+		access_token_key: process.env.AT_KEY,
+		access_token_secret: process.env.AT_SECRET
+	});
+}
+
+// hashtags is an array of objects, hashtag text is [].text
+var results = {};
+
+// TWITTER SEARCH FUNCTION  -URL (PARAM) IN - ARRAY OUT
+
+
+function twitterSearch(since) {
+
+	client.get('search/tweets', {q : '"@founderscoders" #stop OR #go OR #continue', result_type : 'recent', count: '100', since : since} , function(error, tweets, response){
+
+	// POPULATE RESULTS OBJECT
+		tweets.statuses.forEach(function(tweet, index) {
+			// Check if new topic - new if 'retweeted status' property absent
+			if (!tweet.retweeted_status) {
+				// tweet URL: https://twitter.com/{user.screen_name}/status/{id_str}
+				var tweetUrl = 'https://twitter.com/' + tweet.user.screen_name +'/status/' + tweet.id_str ;
+				results[tweet.id] = {text : tweet.text, retweets: tweet.retweet_count, hashtags: tweet.entities.hashtags, url: tweetUrl};
+			}
+			// else tweet is retweet - set retweet count if greater than current
+			else {
+				if (results[tweet.id] && results[tweet.id].retweets < tweet.retweet_count) {
+					results[tweet.id].retweets = tweet.retweet_count;
+				}
+			}
+		});
+	});
+}
+
+setInterval(function(){
+	// since date, format YEAR-MO-DA
+	twitterSearch('2015-02-23');
+},3000);
+
+// SERVER
+
+var server = http.createServer(function(req, res){
+	if (req.url.match(/search/)) {
+		// repackage into stop/go/continue properties
+		var packagedResult = { 'stop' : [], 'go' : [], 'continue' : []};
+		
+		// TODO make this a function
+		for (var key in results) {
+			results[key].hashtags.forEach(function(hashtag, index) {
+				if ( hashtag.text.match(/stop/i) ) {
+					packagedResult.stop.push({ 	id : key, 
+												text : results[key].text, 
+												retweets : results[key].retweets,
+												url : results[key].url });
+				}
+				else if ( hashtag.text.match(/go/i) ) {
+					packagedResult.go.push({ 	id : key, 
+												text : results[key].text, 
+												retweets : results[key].retweets,
+												url : results[key].url });
+				}
+				else if ( hashtag.text.match(/continue/i) ) {
+					packagedResult['continue'].push({	id : key, 
+														text : results[key].text, 
+														retweets : results[key].retweets,
+														url : results[key].url });
+				}
+			});
+		}
+		console.log(results);
+		res.writeHead(200, {'Content-Type': 'application/json'});
+		res.end(JSON.stringify(packagedResult));
+	}
+	else {
+		ecstatic(req, res);
+	}
+});
+
+var currentPort = process.env.PORT || 8000;
+server.listen(currentPort);
+console.log('Server running on port ' + currentPort);
---
+++ public/script.js
@@ -0,0 +1,230 @@
+// TODO: strip @founderscoders and #hashtags from text
+// Make colours proportional to colour space -curenttly, illegal values possible .. something like (255/maxRetweets)*retweets
+// or colorScale?
+// add link to tweet, to retweet...
+
+
+// Mock Data ////////////////////////////////////////////////////////////////////////////
+var mock1 = {
+	'stop' : [{id: 1 , retweets: 3 , text: 'An Idea', url : 'https://twitter.com/JavaScriptDaily/status/570599367331192832'}, {id: 2 , retweets: 2 , text: 'Another Idea', url : 'https://twitter.com/JavaScriptDaily/status/570599367331192832' }, {id: 3 , retweets: 6 , text: 'Not such a good idea', url : 'https://twitter.com/JavaScriptDaily/status/570599367331192832' }, {id: 4 , retweets: 7 , text: 'Yet another idea', url : 'https://twitter.com/JavaScriptDaily/status/570599367331192832' }],
+	'go' : [{id: 1 , retweets: 0 , text: 'An Idea', url : 'https://twitter.com/JavaScriptDaily/status/570599367331192832' }, {id: 2 , retweets: 9 , text: 'Another Idea', url : 'https://twitter.com/JavaScriptDaily/status/570599367331192832' }, {id: 3 , retweets: 0 , text: 'Not such a good idea', url : 'https://twitter.com/JavaScriptDaily/status/570599367331192832' }, {id: 4 , retweets: 1 , text: 'Yet another idea', url : 'https://twitter.com/JavaScriptDaily/status/570599367331192832' }],
+	'continue' : [{id: 1 , retweets: 4 , text: 'An Idea', url : 'https://twitter.com/JavaScriptDaily/status/570599367331192832' }, {id: 2 , retweets: 5 , text: 'Another Idea', url : 'https://twitter.com/JavaScriptDaily/status/570599367331192832' }, {id: 3 , retweets: 3 , text: 'Not such a good idea', url : 'https://twitter.com/JavaScriptDaily/status/570599367331192832' }, {id: 4 , retweets: 3 , text: 'Yet another idea', url : 'https://twitter.com/JavaScriptDaily/status/570599367331192832' }],
+};
+
+var mock2 = {
+	'stop' : [{id: 1 , retweets: 3 , text: 'An Idea', url : 'https://twitter.com/JavaScriptDaily/status/570599367331192832' }, {id: 2 , retweets: 2 , text: 'Another Idea', url : 'https://twitter.com/JavaScriptDaily/status/570599367331192832' }, {id: 3 , retweets: 6 , text: 'Not such a good idea', url : 'https://twitter.com/JavaScriptDaily/status/570599367331192832' }, {id: 4 , retweets: 7 , text: 'Yet another idea', url : 'https://twitter.com/JavaScriptDaily/status/570599367331192832' }, {id: 5 , retweets: 5 , text: 'Yet another idea', url : 'https://twitter.com/JavaScriptDaily/status/570599367331192832' }, {id: 6 , retweets: 2 , text: 'Yet another idea', url : 'https://twitter.com/JavaScriptDaily/status/570599367331192832' }],
+	'go' : [{id: 1 , retweets: 0 , text: 'An Idea', url : 'https://twitter.com/JavaScriptDaily/status/570599367331192832' }, {id: 2 , retweets: 9 , text: 'Another Idea', url : 'https://twitter.com/JavaScriptDaily/status/570599367331192832' }, {id: 3 , retweets: 0 , text: 'Not such a good idea', url : 'https://twitter.com/JavaScriptDaily/status/570599367331192832' }],
+	'continue' : [{id: 1 , retweets: 4 , text: 'An Idea', url : 'https://twitter.com/JavaScriptDaily/status/570599367331192832' }, {id: 2 , retweets: 5 , text: 'Another Idea', url : 'https://twitter.com/JavaScriptDaily/status/570599367331192832' }, {id: 3 , retweets: 3 , text: 'Not such a good idea', url : 'https://twitter.com/JavaScriptDaily/status/570599367331192832' }, {id: 4 , retweets: 3 , text: 'Yet another idea', url : 'https://twitter.com/JavaScriptDaily/status/570599367331192832' }, {id: 5 , retweets: 0 , text: 'Yet another idea', url : 'https://twitter.com/JavaScriptDaily/status/570599367331192832'}, {id: 6 , retweets: 5 , text: 'Yet another idea', url : 'https://twitter.com/JavaScriptDaily/status/570599367331192832' }, {id: 7 , retweets: 12 , text: 'Yet another idea', url : 'https://twitter.com/JavaScriptDaily/status/570599367331192832' }],
+};
+
+var mock3 = {
+	'stop' : [{id: 1 , retweets: 6 , text: 'An Idea', url : 'https://twitter.com/JavaScriptDaily/status/570599367331192832' }, {id: 2 , retweets: 8 , text: 'Another Idea', url : 'https://twitter.com/JavaScriptDaily/status/570599367331192832' }],
+	'go' : [{id: 1 , retweets: 8 , text: 'An Idea', url : 'https://twitter.com/JavaScriptDaily/status/570599367331192832' }, {id: 2 , retweets: 4 , text: 'Another Idea', url : 'https://twitter.com/JavaScriptDaily/status/570599367331192832' }],
+	'continue' : [{id: 1 , retweets: 2 , text: 'An Idea', url : 'https://twitter.com/JavaScriptDaily/status/570599367331192832' }, {id: 2 , retweets: 13 , text: 'Another Idea', url : 'https://twitter.com/JavaScriptDaily/status/570599367331192832' }],
+};
+
+var mocks = [mock1, mock2, mock3];
+var count = 0;
+var currentMock;
+setInterval(function(){
+	currentMock = mocks[count];
+	if (count === 2) {
+		count = 0;
+	}
+	else {
+		count++;
+	} 
+},7000);
+///////////////////////////////////////////////////////////////////////////////////
+
+// Set trafficLight to change dataset
+var trafficLight;
+
+ //Width and height
+var w = 1200;
+var h = 300;
+
+var svg;
+
+function getTweets() {
+	$.getJSON('./search')
+	.done(function(tweets){
+		console.log(tweets);
+		drawChart(currentMock); //(tweets)
+	});
+}
+
+function drawChart(data) {
+
+	var xScale = d3.scale.ordinal()
+						.domain(d3.range(data[trafficLight].length))
+						.rangeRoundBands([0, w], 0.05);
+	
+	// Get array of retweet counts for current chart
+	var retweetList = [];
+	data[trafficLight].forEach(function(tweet, index){
+		retweetList.push(tweet.retweets);
+	});
+
+	// Use array to find max retweet count, and create range for y-axis
+	var yScale = d3.scale.linear()
+						.domain([0, d3.max(retweetList) + 1])
+						.range([0, h]);
+
+	var bars = svg.selectAll("rect").data(data[trafficLight]);
+	var labels = svg.selectAll("text").data(data[trafficLight]);
+	var tweetText = svg2.selectAll('text').data(data[trafficLight]);
+
+	// Add new bars off right
+	bars.enter()
+			.append("rect")
+			.attr({
+				x 				: 	w,
+				y 				: 	function(d) { return h - yScale(d.retweets+1); },
+				width 			: 	xScale.rangeBand(),
+				height 			: 	function(d) { return yScale(d.retweets+1); },
+				class			: 	'target',
+				'tweet-id'		: 	function(d) { return d.id; },
+				'tweet-url'		: 	function(d) { return d.url; },
+				'data-toggle'	: 	'tooltip',
+				'data-container': 	'body',
+				title			: 	function(d) { return d.text; }
+			});
+
+	// Add new text 
+	labels.enter()
+			.append("text")
+			.text(function(d) {
+				return d.retweets;
+			})
+			.attr({
+				x				: 	w,
+				y				: 	function(d) { return h - yScale(d.retweets +1) + 14; },
+				'text-anchor'	: 	'middle',
+				'font-family'	: 	'sans-serif',
+				'font-size'		: 	'16px',
+				fill			: 	'white'
+			});
+
+	tweetText.enter()
+			.append('text')
+			.text(function(d){
+				return d.text +' - ' + d.retweets;
+			})
+			.attr({
+				x 				: 	500,
+				y 				: 	h * 5,
+				class 			: 	'target',
+				'tweet-id'		: 	function(d) { return d.id; },
+				'tweet-url'		: 	function(d) { return d.url; },
+				'font-family'	: 	'sans-serif',
+				'font-size'		: 	'20px',
+				'fill'			: 	'white'
+			});
+
+	// Exit code needed when switching to a smaller dataset
+	 bars.exit()
+		.transition()
+		.duration(500)
+		.attr('x', w)
+		.remove();
+
+	labels.exit()
+		.transition()
+		.duration(500)
+		.attr('x', w)
+		.remove();
+
+	tweetText.exit()
+		.transition()
+		.duration(500)
+		.attr('y', h*5)
+		.remove();
+
+	// Transition elements in, AND alter extant elements in place
+	bars.transition()
+		.duration(500)
+		.attr({
+			x 			: 	function(d, i) { return xScale(i); },
+			y 			: 	function(d) { return h - yScale(d.retweets +1); },
+			width 		: 	xScale.rangeBand(),
+			height		: 	function(d) { return yScale(d.retweets +1); },
+			class		: 	'target',
+			'tweet-id'	: 	function(d){ return d.id; },
+			'tweet-url'	: 	function(d){ return d.url; },
+			fill		: 	function(d) {
+											if (trafficLight === 'go') {
+												return "rgb(0," + (d.retweets * 20) + ",0)";
+											}
+											else if (trafficLight === 'stop') {
+												return "rgb(" + (d.retweets * 20) + ",0,0)";
+											}
+											else if (trafficLight === 'continue') {
+												return "rgb(" + (d.retweets * 20) + "," + (d.retweets * 10) +  ",0)";
+											}
+										}
+		});
+
+
+	labels.transition()
+			.duration(500)
+			.text(function(d) {
+				return d.retweets;
+			})
+			.attr({
+				x 			: 	function(d, i) { return xScale(i) + xScale.rangeBand() / 2; },
+				y 			: 	function(d) { return h - yScale(d.retweets +1) + 14; },
+				width 		: 	xScale.rangeBand(),
+				height		: 	function(d) { return yScale(d.retweets +1); }
+			});
+
+	tweetText.transition()
+			.duration(500)
+			.text(function(d){
+				return d.text +' - ' + d.retweets;
+			})
+			.attr({
+				y 			: 	function(d,i) { return i * 30 + 50; },
+				class 		: 	'target',
+				'tweet-id'	: 	function(d){ return d.id; },
+				'tweet-url'	: 	function(d){ return d.url; }
+			});
+
+	$("body").tooltip({ selector: '[data-toggle=tooltip]' });
+
+}
+
+
+$(document).ready(function(){
+
+	svg = d3.select("body").append("svg").attr( { width : w, height : h, id : 'barchart' });
+
+	svg2 = d3.select("body").append("svg").attr( { width : w, height : h*5 , id : 'tweet-text'});
+
+	// Highlight related bars/text on mouseover
+	var barColor;
+	$(document).on('mouseenter', '.target', function(){
+			var id = $(this).attr('tweet-id');
+			barColor = $('rect[tweet-id='+id+']').attr('fill');
+			$('rect[tweet-id='+id+']').attr('fill', 'rgb(125,0,255)');
+			$('text[tweet-id='+id+']').attr('fill', 'rgb(125,0,255)');
+		});
+
+	$(document).on('mouseleave', '.target', function(){
+			var id = $(this).attr('tweet-id');
+			$('rect[tweet-id='+id+']').attr('fill', barColor);
+			$('text[tweet-id='+id+']').attr('fill', 'white');
+	});
+
+	// Open tweet in new tab on click
+	$(document).on('click', '.target', function(){
+		var tweetUrl = $(this).attr('tweet-url');
+		window.open(tweetUrl);
+	});
+
+	setInterval(function(){
+		trafficLight = $('.chart-select:checked').val();
+		getTweets();
+	},3000);
+
+});
