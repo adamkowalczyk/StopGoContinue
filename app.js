667824111a4773f21ca214b6e7f006d77065682b
Adam Kowalczyk <adamkowalczyk@gmail.com>
2015-02-27 11:46:08 +0000

    error check search

--- app.js
+++ app.js
@@ -46,31 +46,37 @@ var results = {};
 function twitterSearch(since) {
 
 	client.get('search/tweets', {q : '"@founderscoders" #stop OR #go OR #continue', result_type : 'recent', count: '100', since : since} , function(error, tweets, response){
-
+		// console.log(response);
 	// POPULATE RESULTS OBJECT
-		tweets.statuses.forEach(function(tweet, index) {
-			// Check if new topic - new if 'retweeted status' property absent
-			if (!tweet.retweeted_status) {
-				// tweet URL: https://twitter.com/{user.screen_name}/status/{id_str}
-				var tweetUrl = 'https://twitter.com/' + tweet.user.screen_name +'/status/' + tweet.id_str ;
-				// strip hashtags and username from tweet text
-				var tweetText = tweet.text.replace(/(^|\W)(@[a-z\d][\w-]*)|(#[a-z\d][\w-]*)/ig, '' ).trim();
-				results[tweet.id] = {text : tweetText, retweets: tweet.retweet_count, hashtags: tweet.entities.hashtags, url: tweetUrl, user: tweet.user.screen_name};
-			}
-			// else tweet is retweet - set retweet count if greater than current
-			else {
-				if (results[tweet.id] && results[tweet.id].retweets < tweet.retweet_count) {
-					results[tweet.id].retweets = tweet.retweet_count;
+		if (error) {
+			console.log(error);
+			return;
+		}
+		else if (tweets) {
+			tweets.statuses.forEach(function(tweet, index) {
+				// Check if new topic - new if 'retweeted status' property absent
+				if (!tweet.retweeted_status) {
+					// tweet URL: https://twitter.com/{user.screen_name}/status/{id_str}
+					var tweetUrl = 'https://twitter.com/' + tweet.user.screen_name +'/status/' + tweet.id_str ;
+					// strip hashtags and username from tweet text
+					var tweetText = tweet.text.replace(/(^|\W)(@[a-z\d][\w-]*)|(#[a-z\d][\w-]*)/ig, '' ).trim();
+					results[tweet.id] = {text : tweetText, retweets: tweet.retweet_count, hashtags: tweet.entities.hashtags, url: tweetUrl, user: tweet.user.screen_name};
 				}
-			}
-		});
+				// else tweet is retweet - set retweet count if greater than current
+				else {
+					if (results[tweet.id] && results[tweet.id].retweets < tweet.retweet_count) {
+						results[tweet.id].retweets = tweet.retweet_count;
+					}
+				}
+			});
+		}
 	});
 }
 
-setInterval(function(){
-	// since date, format YEAR-MO-DA
-	twitterSearch('2015-02-27');
-},5000);
+// setInterval(function(){
+// 	// since date, format YEAR-MO-DA
+// 	twitterSearch('2015-02-27');
+// },5000);
 
 // SERVER
 
@@ -105,6 +111,7 @@ var server = http.createServer(function(req, res){
 				}
 			});
 		}
+		console.log(packagedResult);
 		res.writeHead(200, {'Content-Type': 'application/json'});
 		res.end(JSON.stringify(packagedResult));
 	}
